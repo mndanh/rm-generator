c593d343ad0c75a607a630e262b094ebcf826478
Dan Heer <mndanh@gmail.com>
2024-07-31 21:51:57 -0500

    added the prompt questions. still need to add licence choices

---
+++ questions.js
@@ -0,0 +1,54 @@
+function questions(){
+    return [
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: 'title',
+        },
+        {
+            type: "input",
+            message: "Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide: - What was your motivation? - Why did you build this project? - What problem does it solve? - What did you learn?",
+            name: 'description',
+        },
+        {
+            type: "input",
+            message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+            name: 'installation',
+        },
+        {
+            type: "input",
+            message: "Provide instructions and examples for use. Include screenshots as needed.",
+            name: 'usage',
+        },
+        {
+            type: "list",
+            message: "What license is this project using??",
+            name: 'license',
+            choices: [],
+        },
+        {
+            type: "input",
+            message: "If you created an application or package and would like other developers to contribute, include guidelines for how to do so.",
+            name: 'contributing',
+        },
+        {
+            type: "input",
+            message: "Go the extra mile and write tests for your application. Then provide examples on how to run them here.",
+            name: 'tests',
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: 'github',
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: 'email',
+        },
+    ]
+};
+
+module.exports = {
+    questions
+};
